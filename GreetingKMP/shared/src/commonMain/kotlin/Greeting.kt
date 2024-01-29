f6a16bede779dcf64bd6ba813ea3448a89513abd
Suguru Takahashi <samonikura100@gmail.com>
2024-01-29 14:28:06 +0900

    #1 チュートリアル

--- GreetingKMP/shared/src/commonMain/kotlin/Greeting.kt
+++ GreetingKMP/shared/src/commonMain/kotlin/Greeting.kt
@@ -1,7 +1,12 @@
+import kotlin.random.Random
+
 class Greeting {
     private val platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+
+        return "$firstWord Guess what this is! > ${platform.name.reversed()}!"
     }
 }
+
